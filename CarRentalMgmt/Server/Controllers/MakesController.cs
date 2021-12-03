e18c3c6dec34fd2c87451475cb6e898e2d54cc21
Barnabas Woon <weiyuann@TP.EDU.SG>
2021-12-03 20:01:44 +0800

    Added REST API Endpoints and Unit of Work pattern

---
+++ CarRentalMgmt/Server/Controllers/MakesController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CarRentalMgmt.Server.Data;
+using CarRentalMgmt.Shared.Domain;
+using CarRentalMgmt.Server.IRepository;
+
+namespace CarRentalMgmt.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MakesController : ControllerBase
+    {
+        //Refractored
+        //private readonly ApplicationDbContext _context;
+        private readonly IUnitOfWork _unitOfWork;
+
+        //Refractored
+        //public MakesController(ApplicationDbContext context)
+        public MakesController(IUnitOfWork unitOfWork)
+        {
+            //Refractored
+            //_context = context;
+            _unitOfWork = unitOfWork;
+        }
+
+        // GET: api/Makes
+        [HttpGet]
+        //Refractored
+        //public async Task<ActionResult<IEnumerable<Make>>> GetMakes()
+        public async Task<IActionResult> GetMakes()
+        {
+            //Refractored
+            //return await _context.Makes.ToListAsync();
+            var makes = await _unitOfWork.Makes.GetAll();
+            return Ok(makes);
+        }
+
+        // GET: api/Makes/5
+        [HttpGet("{id}")]
+        //Refractored
+        //public async Task<ActionResult<Make>> GetMake(int id)
+        public async Task<IActionResult> GetMake(int id)
+        {
+            //Refractored
+            //var make = await _context.Makes.FindAsync(id);
+            var make = await _unitOfWork.Makes.Get(q => q.Id == id);
+
+            if (make == null)
+            {
+                return NotFound();
+            }
+
+            //Refractored
+            //return make;
+            return Ok(make);
+        }
+
+        // PUT: api/Makes/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutMake(int id, Make make)
+        {
+            if (id != make.Id)
+            {
+                return BadRequest();
+            }
+
+            //Refractored
+            //_context.Entry(make).State = EntityState.Modified;
+            _unitOfWork.Makes.Update(make);
+
+            try
+            {
+                //await _context.SaveChangesAsync();
+                await _unitOfWork.Save(HttpContext);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                //Refractored
+                //if (!MakeExists(id))
+                if (!await MakeExists(id))
+                {
+                     return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Makes
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Make>> PostMake(Make make)
+        {
+            //Refractored
+            //_context.Makes.Add(make);
+            //await _context.SaveChangesAsync();
+            await _unitOfWork.Makes.Insert(make);
+            await _unitOfWork.Save(HttpContext);
+
+            return CreatedAtAction("GetMake", new { id = make.Id }, make);
+        }
+
+        // DELETE: api/Makes/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMake(int id)
+        {
+            //Refractored
+            //var make = await _context.Makes.FindAsync(id);
+            var make = await _unitOfWork.Makes.Get(q => q.Id == id);
+            if (make == null)
+            {
+                return NotFound();
+            }
+
+            //Refractored
+            //_context.Makes.Remove(make);
+            //await _context.SaveChangesAsync();
+            await _unitOfWork.Makes.Delete(id);
+            await _unitOfWork.Save(HttpContext);
+
+            return NoContent();
+        }
+
+        //Refractored
+        //private bool MakeExists(int id)
+        private async Task<bool> MakeExists(int id)
+        {
+            //Refractored
+            //return _context.Makes.Any(e => e.Id == id);
+            var make = await _unitOfWork.Makes.Get(q => q.Id == id);
+            return make != null;
+        }
+    }
+}
